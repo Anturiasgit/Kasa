d7e71c1f5e21d4fc1bff6c84116415b0201d9161
Anturia Hassani <anturia.hm@gmail.com>
2025-06-21 00:09:43 +0200

    Arrangement bugs affichages centrage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Banner from '../components/Banner/Banner';
-import Card from '../components/Card/Card';
+
 // Tu importeras ensuite tes données si tu en as (JSON ou API)
 
 function Home() {
@@ -7,15 +7,6 @@ function Home() {
     <div>
       <Banner />
 
-      <section className="cards-container">
-        <Card title="Titre de la location" image="/images/1.jpg" />
-        <Card title="Titre de la location" image="/images/2.jpg" />
-        <Card title="Titre de la location" image="/images/3.jpg" />
-        <Card title="Titre de la location" image="/images/4.jpg" />
-        <Card title="Titre de la location" image="/images/5.jpg" />
-        <Card title="Titre de la location" image="/images/6.jpg" />
-        {/* Répète pour tous tes logements ou map sur un tableau */}
-      </section>
     </div>
   );
 }
