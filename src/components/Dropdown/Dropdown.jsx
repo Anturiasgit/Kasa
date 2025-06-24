95d8af643d3370fc350a0682ca8dfb8089d92312
Anturia Hassani <anturia.hm@gmail.com>
2025-06-24 14:24:14 +0200

    Page à propos terminée

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,14 +1,24 @@
 import './Dropdown.scss';
+import { useState } from 'react';
 
+function Dropdown({title,content}) {
+    const [rotated, setRotated] = useState(false);
+    const [displayed, setDisplayed] = useState(false);
 
-function Dropdown() {
+    const handleClick = () => {
+            setRotated(prev => !prev);
+            setDisplayed(prev => !prev);
+    };
+
     return (
         <div className="dropdown">
             <div className="title">
-                <h1>Fiabilité</h1>
+                <h1>{title}</h1>
+                <img src="/arrow_up.png" alt="Flèche pour le dropdown" 
+                className={`${rotated ? 'rotated' : ''}`} onClick={handleClick}/>
             </div>
-            <div className="content">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, fugiat! Quo expedita aperiam explicabo dolore reiciendis porro praesentium quis similique cumque ipsam, animi a inventore dolorum commodi hic vitae laborum.</p>
+            <div className={`content ${displayed ? 'displayed' : ''}`}>
+                <p>{content}</p>
             </div>
         </div>
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,16 @@
 import Banner from '../components/Banner/Banner'
 import imgAbout from '../assets/image_about.png'
 import Dropdown from '../components/Dropdown/Dropdown'
-// import dropdowns from '../dropdowns.json'
+import dropdowns from '../../dropdowns.json'
 
 function About() {
   return ( 
     <div>
       <Banner image={imgAbout} alt="Photo d'une montagne"/>
 
-      <Dropdown />
-      {/* <Dropdown title={dropdown.title} content={dropdown.content}/> */}
+      {dropdowns.map((dropdown) => (
+      <Dropdown title={dropdown.title} content={dropdown.content}/> 
+      ))}
 
     </div>
   )
