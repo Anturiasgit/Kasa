37065f749c3b67922d176099051df297d1af6c7d
Anturia Hassani <anturia.hm@gmail.com>
2025-07-01 12:01:04 +0200

    Renommage fichiers

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,15 @@
 import Banner from '../components/Banner/Banner'
 import imgAbout from '../assets/image_about.png'
 import Collapse from '../components/Collapse/Collapse'
-import collapses from '../../collapses.json'
+import about from '../../about.json'
 
 function About() {
   return ( 
     <div>
       <Banner className="about-banner" image={imgAbout} alt="Photo d'une montagne"/>
 
-      {collapses.map((collapse) => (
-      <Collapse title={collapse.title} content={collapse.content}/> 
+      {about.map((ab) => (
+      <Collapse title={ab.title} content={ab.content}/> 
       ))}
 
     </div>
